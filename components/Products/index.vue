3dbfb6fd6663b8b1a82efa42bfda04698078525f
Muhammad Bilal Bacha <bilal.bacha@sigmads.com>
2023-11-10 15:42:21 +0500

    Add Loader

--- components/Products/index.vue
+++ components/Products/index.vue
@@ -3,10 +3,13 @@
         <div class="flex justify-center" v-for="product in products">
             <ProductsCard :products="product" />
         </div>
-       <div class="" v-if="products.length == 0">
-        <h1>No Products Found !!</h1>
-       </div>
+       
     </div>
+    <div class="flex justify-center items-center w-full" v-if="products.length == 0" >
+        
+        <div class="loader flex justify-center"></div>
+
+   </div>
 </template>
 
 <script setup lang="ts">
@@ -22,5 +25,18 @@ onBeforeMount(() => {
 </script>
 
 <style scoped>
+.loader {
+  border: 5px solid #f3f3f3; /* Light grey background */
+  border-top: 5px solid red; /* Red color for the loader */
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 
 </style>
